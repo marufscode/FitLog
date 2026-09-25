1723a93657015c880c56e0104832c5fe56e85036
SUNDARBAN IT <mdmarufshamim015.com>
2026-09-25 21:54:27 +0600

    find bug and fix nav logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/asset";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { usePlan } from "@/context/PlanContext";
 
